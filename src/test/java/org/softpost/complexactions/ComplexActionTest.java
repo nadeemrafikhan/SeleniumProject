a297175ef9469ca2edac957d953227bb193ca12b
nadeemrafikhan <40570064+nadeemrafikhan@users.noreply.github.com>
2019-08-29 09:22:21 +0530

    Update ComplexActionTest.java

--- src/test/java/org/softpost/complexactions/ComplexActionTest.java
+++ src/test/java/org/softpost/complexactions/ComplexActionTest.java
@@ -17,7 +17,7 @@ public class ComplexActionTest {
 
     @Test
     public void testLaunch() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sagar\\Softwares\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         //Maximize
