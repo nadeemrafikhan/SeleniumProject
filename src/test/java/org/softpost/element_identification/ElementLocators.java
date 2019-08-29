a3bb145ab9f337b57e82d3bf096f7f07285c0da7
nadeemrafikhan <40570064+nadeemrafikhan@users.noreply.github.com>
2019-08-29 09:24:05 +0530

    Update ElementLocators.java

--- src/test/java/org/softpost/element_identification/ElementLocators.java
+++ src/test/java/org/softpost/element_identification/ElementLocators.java
@@ -13,7 +13,7 @@ public class ElementLocators {
 
     @Test
     public void testLaunch() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sagar\\Softwares\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         //Maximize
