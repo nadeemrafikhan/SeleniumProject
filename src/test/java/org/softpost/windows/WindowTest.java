14cfaad42be5772305238e914abea5fea51bca7a
nadeemrafikhan <40570064+nadeemrafikhan@users.noreply.github.com>
2019-08-29 09:31:54 +0530

    Update WindowTest.java

--- src/test/java/org/softpost/windows/WindowTest.java
+++ src/test/java/org/softpost/windows/WindowTest.java
@@ -21,7 +21,7 @@ public class WindowTest {
 
     @Test
     public void testLaunch() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sagar\\Softwares\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         //Maximize
